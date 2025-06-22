b264bdde9d44d10e88e19fa35449c67a0a914678
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-22 18:22:41 +0530

    dictionary

--- python/revision/dictionary/01_dictionary1.py
+++ python/revision/dictionary/01_dictionary1.py
@@ -9,4 +9,17 @@ print(chai_types)
 for chai in chai_types:
     print(chai, chai_types[chai])
     
+    print(chai_types.get("masala"))
 
+chai_types["ginger"] = "zesty"
+print(chai_types)
+
+
+for chai in chai_types:
+    print(chai)
+for chai in chai_types:
+    print(chai,chai_types[chai])
+
+
+for key,value in chai_types.items():
+    print(key,values)
