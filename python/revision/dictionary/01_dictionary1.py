16f9093075b5bf2b6191b6ef39b4adda442a8bbd
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-22 17:33:27 +0530

    squared

---
+++ python/revision/dictionary/01_dictionary1.py
@@ -0,0 +1,12 @@
+chai_types = {"masala":"spicy","ginger":"zesty","green":"mild"}
+
+print (chai_types["masala"])
+
+(chai_types["green"]) = "fresh"
+
+print(chai_types)
+
+for chai in chai_types:
+    print(chai, chai_types[chai])
+    
+
