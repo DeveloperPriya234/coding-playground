88e1f6689d57f494a9e5f88a044714e476d724db
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-23 12:34:55 +0530

    dictionary3

---
+++ python/revision/number-depth/03_dictionary3.py
@@ -0,0 +1,27 @@
+chai_types = {"masala":"spicy","ginger":"zesty","green":"fresh"}
+
+chai_types["earl grey"]=  "citrus"
+print(chai_types)
+
+chai_types.pop("ginger")
+print(chai_types)
+
+chai_types["ginger"]= "zesty"
+print(chai_types)
+
+print(chai_types.pop("ginger"))
+print(chai_types)
+
+(chai_types.popitem())
+print(chai_types)
+
+chai_types["oolong"]= "smooth"
+print(chai_types)
+chai_types["ginger"]="zesty"
+
+print(chai_types)
+
+chai_types.popitem()
+print(chai_types)
+print(len(chai_types))
+
