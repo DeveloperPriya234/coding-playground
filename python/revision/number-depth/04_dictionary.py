3d1237a44cea27d43a5b1e1e5b6f282d8c86d7c9
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-23 18:14:13 +0530

    4dictionary

---
+++ python/revision/number-depth/04_dictionary.py
@@ -0,0 +1,6 @@
+tea_shop = {"chai":{"masala":"spicy","ginger":"zesty"},"tea":{"green":"fresh","black":"strong"}}
+
+print(tea_shop)
+print(tea_shop["chai"])
+print(tea_shop["tea"])
+print(tea_shop["chai"]["masala"])
