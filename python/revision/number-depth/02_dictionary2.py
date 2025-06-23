1b754722e591da7b36fe2cdf4a0a19742564c21a
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-23 11:37:46 +0530

    rwvision dictionary

--- python/revision/number-depth/02_dictionary2.py
+++ python/revision/number-depth/02_dictionary2.py
@@ -7,3 +7,19 @@ print(chai_types)
 print(chai_types["masala"])
 
 print(chai_types.get("masala"))
+
+print(chai_types.get("oolong","not found"))
+
+
+for chai in chai_types:
+    print(chai)
+    
+for chai in chai_types:
+    print(chai,chai_types[chai])
+    
+for key,value in chai_types.items():
+    print(key,value)
+    if "masala" in chai_types:
+        print(" i have masala in chai_types")
+else:
+    print(" i dont have masala in chai_types")
