6002d06abc20ff6141cd15b9511c058aa9e4117a
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-23 19:52:41 +0530

    revision dictioanary

--- python/revision/number-depth/5_dictionary5.py
+++ python/revision/number-depth/5_dictionary5.py
@@ -17,3 +17,17 @@ chai_types.pop("ginger")
 print(chai_types)
 
 print(chai_types.popitem())
+
+
+if "oolong" in chai_types:
+    print("oolong is in the chai_types")
+    print(chai_types)
+    chai_types["green"]= "fresh"
+    print(chai_types)
+    chai_types["masala"]="spicy"
+    print(chai_typess)
+    
+    
+    
+    if "oolong"in chai_types:
+        print("oolong in chai_types")
