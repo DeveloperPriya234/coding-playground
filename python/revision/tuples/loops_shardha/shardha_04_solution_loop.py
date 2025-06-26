766fece02856949ef52dfb23c0a55c68ca334114
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-26 21:34:31 +0530

    shardha  lopp solution4

---
+++ python/revision/tuples/loops_shardha/shardha_04_solution_loop.py
@@ -0,0 +1,15 @@
+#"print the elements of the following list usimg a loop"
+
+
+
+nums = (1,4,9,16,25,36,49,64,81,100)
+heroes = ("ironman","thor","superman","baatman")
+# idx = 0
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx += 1
+    
+idx = 0
+while idx < len(heroes):
+    print(heroes[idx])
+    idx += 1
