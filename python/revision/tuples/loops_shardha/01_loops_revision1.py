9456987ed99284c46536c7d4c68aae0c7ffb9160
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-26 15:45:58 +0530

    shardha loop revision

---
+++ python/revision/tuples/loops_shardha/01_loops_revision1.py
@@ -0,0 +1,12 @@
+# while True:
+#     print("hello world")
+count = 1
+while count <= 5:
+    print("hello")
+    count += 1
+    
+count = 1
+while count <= 5:
+    print("hello")
+    count += 1
+print("ended loop")
