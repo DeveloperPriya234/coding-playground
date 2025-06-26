10ce763f6ac5126a146fd2bd4deb5c11f6b219e7
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-26 20:18:59 +0530

    shardha_03solution

---
+++ python/revision/tuples/loops_shardha/shardha_03)solution3.py
@@ -0,0 +1,53 @@
+# while True:
+#     print("hello world")
+# while True:
+#     print("hello world")
+# count = 1
+# while count <= 5:
+#     print("hello")
+#     count += 1
+    
+# count = 1
+# while count <= 5:
+#     print("hello")
+#     count += 1
+# print("ended loop")
+
+
+# i = 5
+# while i >= 1:
+#     print("hello world")
+#     i -= 1
+# print("loop ended")
+
+
+
+
+
+
+
+n = int(input("enter number:"))
+i =1 
+while i <= 10:
+    print(n*i)
+    i += 1
+print("here table of",n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+i = 1
+while i <= 10:
+    print(17*i)
+    i += 1
+print("here table of 3 on yours screen")
