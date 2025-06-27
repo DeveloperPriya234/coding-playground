695d009b677c6fd83cb6975f7de4d979dd174aac
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-27 20:04:28 +0530

    loops6

---
+++ python/revision/tuples/loops_chai_aur_code/06_loops6.py
@@ -0,0 +1,7 @@
+#compute the factorial of a number using a while loop:
+number = 5
+factorial = 1
+while number > 0:
+    factorial = factorial * number
+    number = number - 1
+    print("factorial number of:",factorial)
