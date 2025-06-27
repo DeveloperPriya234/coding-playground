9c024ddf51c138711179806b698e430b4a47d835
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-27 18:25:33 +0530

    loops4

---
+++ python/revision/tuples/loops_chai_aur_code/04_loop4.py
@@ -0,0 +1,13 @@
+input_str = "python"
+reversed_str = ""
+
+for char in input_str:
+    reversed_str =  reversed_str + char
+    
+print(reversed_str)
+
+input_char = "python"
+reversed_str = ""
+for char in input_str:
+    reversed_str = char + reversed_str
+print(reversed_str)
