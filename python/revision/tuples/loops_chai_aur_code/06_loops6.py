de3bca42094ea66f55cac55fb8f5310efeb3d2a8
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-27 20:08:31 +0530

    6loop

--- python/revision/tuples/loops_chai_aur_code/06_loops6.py
+++ python/revision/tuples/loops_chai_aur_code/06_loops6.py
@@ -2,6 +2,6 @@
 number = 5
 factorial = 1
 while number > 0:
-    factorial = factorial * number
-    number = number - 1
+    factorial *= number
+    number -= 1
     print("factorial number of:",factorial)
