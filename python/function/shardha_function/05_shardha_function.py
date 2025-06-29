db9b76b3f4afc9fbd1ad4bc0d30b8cda1ef790a3
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-29 17:04:59 +0530

    05_shardhafunction5

---
+++ python/function/shardha_function/05_shardha_function.py
@@ -0,0 +1,44 @@
+  #functions in python:
+#   built in python   and user defined ppython
+  
+  
+#   built in python:
+# print()
+# len()
+# type()
+# range()
+
+# user defined functions:
+    
+
+      
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+# print("apnacollege","shardha")
+# print("apna college",end =" ")#sep = ""
+
+
+
+
+
+# len()
+#(function) def len(_ob: sized,/)
+
+# -> int
+# return the number of item in a container(integer)
+
+# range()
+# class range(
+# _stop: supportindex,
+# /
+# ): ...
+# class range(_start: supportindex,
+#             _stop: supportindex,
+#             _step: supportindex = ...,/): ...
+
+# range(stop) -> range object range(start,stop[step]) -> range object
