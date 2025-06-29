2618470cfd619964c5822ad12d1f167185c2f401
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-29 21:16:24 +0530

    solution2 functtion(shardha)

--- python/function/02_solution2_s.py
+++ python/function/02_solution2_s.py
@@ -1,10 +1,38 @@
 cities = ["delhi","gurgaon","noida","pune","mumbai","chennai"]
 heroes = ["thor","ironman","captain america","shakti man"]
+# print("heroes",end=":")
+
+# print(heroes[0],
+# end =" ")
+# print(heroes[1],
+# end =" ")
+# print(heroes[2],
+# end =" ")
+# print(heroes[3],
+# end =" ")
+
+# print("cities",end = ":")
+
+# print(cities[0],
+# end =" ")
+# print(cities[1],
+# end =" ")
+# print(cities[2],
+# end =" ")
+# print(cities[3],
+# end =" ")
+# print(cities[4],
+# end =" ")
+# print(cities[5],
+# end =" ")
+
+def print_list(list):
+    for item in list:
+        print(item,end=" , ")
+        
+print_list(heroes)
+print_list(cities)
 
 
-def print_len(list):
-    print(len(list))
-    
-    print_len(cities)
 
-    print_len(heroes)
+ 
