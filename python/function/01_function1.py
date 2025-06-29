71eb1e9bfd1a0404d0e062d16cd69d3042368703
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-29 12:06:13 +0530

    functionshardha

---
+++ python/function/01_function1.py
@@ -0,0 +1,9 @@
+#basic function syntax
+#problem: write a function to calculate and return the square of a number.
+
+
+def square(number):
+    print(number ** 2)
+    
+result = square(4)
+print(result)
---
+++ python/function/shardha_function/01_function_shardha.py
@@ -0,0 +1,30 @@
+a = 5
+b = 10
+sum = a + b
+print(a+b)
+
+# more lines of codes
+a = 2
+b = 10
+
+sum = a+ b
+print(a+b)
+
+#more lines code
+a = 12
+b = 17
+
+sum = a+ b
+print(a+b)
+#more lines code
+a = 34
+b = 45
+
+sum = a+ b
+print(a+b)
+
+#more lines code
+a = 93
+b = 23
+sum = a+b
+print(a+b)
