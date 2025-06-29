a34d58bef1eb86be08428ce7897af41ca530af2d
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-29 23:35:40 +0530

    01_solution_function_chai

---
+++ python/function/chai_aur_code_function/01_solution1.py
@@ -0,0 +1,11 @@
+#basic function syntax:
+# write a function to calculate and return the square of a number
+
+def square_num(number):
+    square = number**2
+    print(square)
+    return square
+    
+    
+result = square_num(7)
+print(result)
