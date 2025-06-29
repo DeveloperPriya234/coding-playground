64c6110e3a0527fff5f51a91658ec8caca06432c
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-29 13:44:11 +0530

    3function_shardha

---
+++ python/function/03_sunction_shardha3.py
@@ -0,0 +1,28 @@
+#function difination
+def calc_sum(a,b): # function perameter
+    sum = a+b
+    print(a+b)
+    return sum
+
+calc_sum(34,23)#function call; arguments
+calc_sum(28,82)#function call;arguments
+
+def calc_sum(a,b):
+    return a + b
+
+sum = calc_sum(23,45)
+print(sum)
+
+def print_hello():
+    print("hello")
+    
+print_hello()
+print_hello()
+print_hello()
+print_hello()
+print_hello()
+
+def print_hello():
+    print("hello")
+output = print_hello()
+print(output)#none
