76ddef7312001bccbbbeae82befe45b6f3cb4728
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-30 00:08:24 +0530

    03_function_chai

---
+++ python/function/02_solution2.py
@@ -0,0 +1,9 @@
+#function with multiple parameters:
+# problem: create a function the takes two numbers as parameter and return their sum
+
+def calc_sum(a,b):
+    sum = a + b
+    print(sum)
+    return sum
+
+calc_sum(2,3)
---
+++ python/function/03_solution_function3.py
@@ -0,0 +1,9 @@
+#polymorphism in functions
+# prpblem:write a function multiply tnat multiplies two number,but can also accept aand multiply strings
+
+def multiply(p1,p2):
+    multiply = p1* p2
+    return multiply
+
+print(multiply("h",3))
+    
