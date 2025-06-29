9ecaec87b67af91ebca996b925c13062da2c9b93
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-29 20:51:04 +0530

    function_solutipon1.py(by shardha

---
+++ python/function/02_solution2_s.py
@@ -0,0 +1,10 @@
+cities = ["delhi","gurgaon","noida","pune","mumbai","chennai"]
+heroes = ["thor","ironman","captain america","shakti man"]
+
+
+def print_len(list):
+    print_len(list)
+    
+    print_len(cities)
+    
+    print_len(heroes)
