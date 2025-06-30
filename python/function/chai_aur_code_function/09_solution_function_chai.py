c82d2cc78c2980935f313f1a1d530d7e131e5d89
DeveloperPriya234 <developerpriya142@gmail.com>
2025-06-30 19:57:29 +0530

    09_solution_function(chai)

--- python/function/chai_aur_code_function/09_solution_function_chai.py
+++ python/function/chai_aur_code_function/09_solution_function_chai.py
@@ -1,2 +1,21 @@
 #generate function with yield:
 # problem:while a generator function the yields even numbers up to a specified limit.
+
+def even_generator(limit):
+    list=[]
+
+    for i in  range(2,limit+1,2):
+        
+        list.append(i)
+    return list
+      
+print(even_generator(10))
+      
+      
+def even_generator(limit):
+    for i in range(2,limit+1,2):
+        yield i
+        
+        
+for num in even_generator(10):
+    print(num)
