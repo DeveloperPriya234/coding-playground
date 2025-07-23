bf41c967f6f5e3e751be21121a3deab819f64a2d
DeveloperPriya234 <developerpriya142@gmail.com>
2025-07-23 11:13:56 +0530

    prime

--- D.S.A/basic_maths/prime.py
+++ D.S.A/basic_maths/prime.py
@@ -1,18 +1,19 @@
 # given an integer check if it is prime or not prime true if number is prime .print false
 
 
-num = 55
-is_prime = True
+# Simple prime number check using a for loop
+
+num = 100
 
 if num <= 1:
-    is_prime = False
+    print(num, "is not a prime number.")
 else:
+    is_prime = True
     for i in range(2, num):
         if num % i == 0:
             is_prime = False
             break
-``
-if is_prime:
-    print("Prime number")
-else:
-    print("Not a prime number")
+    if is_prime:
+        print(num, "is a prime number.")
+    else:
+        print(num, "is not a prime number.")
