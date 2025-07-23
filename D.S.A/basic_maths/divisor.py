cb1b9d7bb44b3976b180271f5ee79b9a1b197812
DeveloperPriya234 <developerpriya142@gmail.com>
2025-07-23 10:44:24 +0530

    divisor

--- D.S.A/basic_maths/divisor.py
+++ D.S.A/basic_maths/divisor.py
@@ -5,4 +5,14 @@ for i in range(1,n+1):
     if n%i == 0:
         print(i)
             
+def print_divisors(n):
+    print(f"Divisors of {n} are:")
+    for i in range(1, n + 1):
+        if n % i == 0:
+            print(i)
+
+# Example usage
+number = 65
+
+print_divisors(number)
             
