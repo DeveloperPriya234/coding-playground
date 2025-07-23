9ab644db11478d0bbd8e6f525f850e12d318d6a5
DeveloperPriya234 <developerpriya142@gmail.com>
2025-07-23 16:54:08 +0530

    gcd and hcf

--- D.S.A/basic_maths/gcd and hcf.py
+++ D.S.A/basic_maths/gcd and hcf.py
@@ -1,26 +1,24 @@
-import math
+# import math
 
-def find_hcf(a, b):
-    return math.gcd(a, b)
+# def find_hcf(a, b):
+#     return math.gcd(a, b)
 
-# Example
-num1 = 36
-num2 = 60
-print("HCF is:", find_hcf(num1, num2))
-
-
-
-def find_hcf(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-# Example
-num1 = 36
-num2 = 60
-print("HCF is:", find_hcf(num1, num2))
+# # Example
+# num1 = 36
+# num2 = 60
+# print("HCF is:", find_hcf(num1, num2))
 
 
+
+# def find_hcf(a, b):
+#     while b != 0:
+#         a, b = b, a % b
+#     return a
+
+# # Example
+# num1 = 36
+# num2 = 60
+# print("HCF is:", find_hcf(num1, num2))
 
 
 
@@ -28,14 +26,29 @@ print("HCF is:", find_hcf(num1, num2))
 
 
 
-def find_hcf(a, b):
+
+
+# def find_hcf(a, b):
+#     hcf = 1
+#     for i in range(1, min(a, b) + 1):
+#         if a % i == 0 and b % i == 0:
+#             hcf = i
+#     return hcf
+
+# # Example
+# num1 = 36
+# num2 = 60
+# print("HCF is:", find_hcf(num1, num2))
+
+
+def find_gcd(a,b):
     hcf = 1
-    for i in range(1, min(a, b) + 1):
-        if a % i == 0 and b % i == 0:
-            hcf = i
-    return hcf
-
-# Example
-num1 = 36
-num2 = 60
-print("HCF is:", find_hcf(num1, num2))
+    for i in range(1,min(a,b)+1):
+        if a % i == 0 and b%i == 0:
+            gcd = i
+    return gcd
+num1 = 3
+num2 = 9
+print("gcd is:",find_gcd(num1,num2))
+            
+    
