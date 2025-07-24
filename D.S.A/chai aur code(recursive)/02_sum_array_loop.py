a10a2e246ece5783fb04c9f42e7b9a8da1bbd75e
DeveloperPriya234 <developerpriya142@gmail.com>
2025-07-24 21:39:04 +0530

    sum_recursive

--- D.S.A/chai aur code(recursive)/02_sum_array_loop.py
+++ D.S.A/chai aur code(recursive)/02_sum_array_loop.py
@@ -1,16 +1,16 @@
-# def sum_iterative(arr):
-#     total = 0
-#     for num in arr:
-#         total += num
-#         return total
-# print(sum_iterative([1,2,3,4,5,67,8,9,10]))
+# # def sum_iterative(arr):
+# #     total = 0
+# #     for num in arr:
+# #         total += num
+# #         return total
+# # print(sum_iterative([1,2,3,4,5,67,8,9,10]))
 
 
-# def sum_two_numbers(a,b,c):
-#     return a+b+c
+# # def sum_two_numbers(a,b,c):
+# #     return a+b+c
 
-# result = (sum_two_numbers(1,2,3))
-# print("sum  of two numbers:",result)
+# # result = (sum_two_numbers(1,2,3))
+# # print("sum  of two numbers:",result)
 
 
 
