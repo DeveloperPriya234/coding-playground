eb28e5dbb566982eac9e8803de7c42f73cbdda10
DeveloperPriya234 <developerpriya142@gmail.com>
2025-07-24 19:14:49 +0530

    recursive

--- D.S.A/chai aur code(recursive)/01_recursion_cc.py
+++ D.S.A/chai aur code(recursive)/01_recursion_cc.py
@@ -6,3 +6,16 @@ def print_numbers(Lrange,Urange):
     print_numbers(Lrange+1,Urange)
     
 print_numbers(1,10)
+
+
+
+def print_numbers(Lrange,Urange):
+    #base case
+    if Lrange > Urange:
+        return
+    print_numbers(Lrange+1,Urange)
+    print(Lrange)
+print_numbers(1,10)
+
+
+
