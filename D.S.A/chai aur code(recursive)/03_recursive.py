810ec18f5efeb175b49b3e901f31028f5baf2492
DeveloperPriya234 <developerpriya142@gmail.com>
2025-07-25 10:50:05 +0530

    iterative

---
+++ D.S.A/chai aur code(recursive)/03_recursive.py
@@ -0,0 +1,6 @@
+def sum_itterative(arr):
+    total = 0
+    for num in arr:
+        total += num
+    return total
+print(sum_itterative([1,2,3,4,5,6,7,8,9,10]))
---
+++ D.S.A/chai aur code(recursive)/04_recursive_loop.py
@@ -0,0 +1,6 @@
+def sum_iterative(arr):
+    total = 0
+    for num in arr:
+        total += num
+    return total
+print(sum_iterative([1,2,3,4,5,6,7,8,9,10]))
