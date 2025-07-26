452d408cb49f35ef0c1f5c0cf3d4191bf3140bd7
DeveloperPriya234 <developerpriya142@gmail.com>
2025-07-26 16:25:46 +0530

    factorial

--- D.S.A/chai aur code(recursive)/factorial.py
+++ D.S.A/chai aur code(recursive)/factorial.py
@@ -4,3 +4,10 @@ def find_factorial(n):
         result *= i
     return result
 print(find_factorial(8))
+
+def factorial_recursive(n):
+    if n ==0 or n==1:
+        return 1
+    else:
+        return n * factorial_recursive(n-1)
+print(factorial_recursive(4))
