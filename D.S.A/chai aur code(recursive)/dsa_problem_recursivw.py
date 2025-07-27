6834286579e43b1d566a7984971e354f524a6e34
DeveloperPriya234 <developerpriya142@gmail.com>
2025-07-27 20:19:55 +0530

    prin name

--- D.S.A/chai aur code(recursive)/dsa_problem_recursivw.py
+++ D.S.A/chai aur code(recursive)/dsa_problem_recursivw.py
@@ -5,3 +5,12 @@
 #9but by bacend react)
 #print n to 1
 #print from n to 1(beektrouble)
+def print_name(name, times):
+    if times <= 0:
+        return
+    print(name)
+    print_name(name, times - 1)
+
+# Example usage
+n = 5
+print_name("priya",  n)
