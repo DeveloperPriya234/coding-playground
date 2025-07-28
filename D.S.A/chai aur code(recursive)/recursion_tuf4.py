74aab566ada245df4fe7344397b83f72de173854
DeveloperPriya234 <developerpriya142@gmail.com>
2025-07-28 10:46:22 +0530

    print n to 1 using (lrange -1)

--- D.S.A/chai aur code(recursive)/recursion_tuf4.py
+++ D.S.A/chai aur code(recursive)/recursion_tuf4.py
@@ -9,3 +9,15 @@ def print_numbers(Lrange,Urange):
         print_numbers(Lrange-1,Urange)
 print(print_numbers(10,1))
     
+    
+    #print from 1 to n(dont allow to use (i+1,n)
+
+def print_numbers(Lrange,Urange):
+    if Lrange < Urange:
+        return
+    else:
+        print_numbers(Lrange-1,Urange)
+        print(Lrange)
+        
+print(print_numbers(10,1))
+    
