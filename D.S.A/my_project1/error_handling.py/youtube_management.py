3cc49aeb6d0d42601d51b07524652f7e0a51ee43
DeveloperPriya234 <developerpriya142@gmail.com>
2025-08-04 17:30:53 +0530

    commit

---
+++ D.S.A/my_project1/error_handling.py/youtube_management.py
@@ -0,0 +1,60 @@
+
+import json
+
+def load_data():
+    pass
+
+def list_all_videos(videos):
+    pass
+def add_video(videos):
+    pass
+
+def update_video(videos):
+    pass
+def delete_video(videos):
+    pass
+
+
+
+
+
+def main():
+    videos = []
+    
+
+
+while True:
+    print("/n youtube manager | choose an option")
+    print("1.list a all youtube videos")
+    print("2.add youtube video")
+    print("3.update a youtube video details")
+    print("4.delete a youtube video")
+    print("5.exit the app")
+    
+    choice =  input("enter your choice")
+    
+    match choice:
+        case "1":
+            list_all_videos(videos)
+        
+        case "2":
+            add_video(videos)
+            
+        case "3":
+            update_video(videos)
+            
+        case "4":
+            delete_video(videos)
+            
+        case "5":
+            break
+        
+        case _:
+            print("invalid choice")
+            
+
+
+
+if __name__ == "__main__":
+    main()
+    
