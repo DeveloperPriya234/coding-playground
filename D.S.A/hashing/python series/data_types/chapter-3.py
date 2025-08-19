b5d72f5127828a2264bf476786577ea0dcea0caf
DeveloperPriya234 <developerpriya142@gmail.com>
2025-08-19 21:09:01 +0530

    chapter 3

---
+++ D.S.A/hashing/python series/data_types/chapter-3.py
@@ -0,0 +1,18 @@
+#integer
+
+black_tea_grams = 14
+ginger_grams = 3
+
+total_grams = black_tea_grams+ ginger_grams
+print(f"Total grams of base tea: {total_grams}")
+
+remaining_tea = black_tea_grams - ginger_grams
+print(f"total grams of base tea :{remaining_tea}")
+
+
+
+
+milk_litres = 7 
+serving = 4
+milk_per_serving = milk_litres/serving
+print(f"milk per serving: {milk_per_serving}")
---
+++ D.S.A/hashing/python series/data_types/chaptter_2.py
@@ -0,0 +1,8 @@
+spice_mix = set()
+print(f"initial spice mix id:{id(spice_mix)}")
+print(f"initial spice mix id:{spice_mix}")
+spice_mix.add("ginger")
+spice_mix.add("cardamon")
+
+print(f"after spice mix id:{spice_mix}")
+print(f"after spice mix id:{id(spice_mix)}")
