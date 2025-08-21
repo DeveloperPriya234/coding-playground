ef14b44f3eb55345add886c57bd06c0b264fb350
DeveloperPriya234 <developerpriya142@gmail.com>
2025-08-21 18:23:38 +0530

    boolean chapter 4

---
+++ D.S.A/hashing/python series/data_types/chapter_4.py
@@ -0,0 +1,16 @@
+is_boiling = True # note : true is 1 and false is consider as 0
+stri_count = 5
+total_actions = stri_count + is_boiling # upcasting
+print(f"total actions:{total_actions}")
+
+
+milk_presents = None# no milk
+print(f"is there milk?{bool(milk_presents)}")
+
+milk_presents = 0# no milk
+print(f"is there milk : {bool(milk_presents)}")
+
+water_hot = True
+tea_added = False
+can_server = water_hot and tea_added
+print(f"can serve chai? {can_server}")
