322f67cd3807febad70b8acdde5e8a350ae6a45a
DeveloperPriya234 <developerpriya142@gmail.com>
2025-08-23 19:14:34 +0530

    strings

---
+++ D.S.A/hashing/python series/chapter6.py
@@ -0,0 +1,10 @@
+chai_types = "ginger chai"
+customer_name = "Priya"
+
+print(f"order for{customer_name}:{chai_types} please !")
+chai_description = "aromatic and bold"
+
+
+print(f"first word:{chai_description[0:8:2]}")# 1 means every character 2. means every 2 character'
+print(f"first word:{chai_description[:8]}")
+print(f"last word:{chai_description[2:]}")
