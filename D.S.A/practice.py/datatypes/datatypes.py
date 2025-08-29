47f2615ba35da4646ef7acfc600fcdde42b4cfaa
DeveloperPriya234 <developerpriya142@gmail.com>
2025-08-29 19:22:23 +0530

    html pattern

---
+++ D.S.A/practice.py/datatypes/datatypes.py
@@ -0,0 +1,47 @@
+#data types
+# integerss 
+# strings
+#floats
+# booleans
+# lists
+# tuples
+# sets
+# dictionaries
+#python is case sensetive A and small a are diffrent
+
+
+
+print(type(10)) #integer
+print(type(10.5)) #float
+print(type("hello")) #string
+print(type(True)) #boolean
+print(type([1,2,3])) #list
+print(type((1,2,3))) #tuple
+print(type({1,2,3})) #set
+print(type({"name":"john", "age":30})) #dictionary
+
+
+
+
+# types of operators
+
+# an operators is a symbol that performs a certain operation between operands
+
+# arithmetic operators
+# + - * / % // **
+# comparison operators
+# == != > < >= <=
+# logical operators
+# and or not
+# assignment operators
+# = += -= *= /= %= //= **=
+
+#logical operators(not,and,or)
+
+
+#?arithmetic operators
+a = 10
+b = 5
+
+sum= a+b
+print(f"sum:",sum)
