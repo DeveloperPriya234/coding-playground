1618362cf00b66aed173e54a55a4fd41897f7d93
DeveloperPriya234 <developerpriya142@gmail.com>
2025-10-04 18:59:13 +0530

    js

---
+++ D.S.A/javascript/01_basics/01_variables.js
@@ -0,0 +1,19 @@
+
+const accountId= 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 //not allow
+accountEmail="hc@hc.com"
+accountPassword="54321"
+accountCity = "punjab"
+
+console.log(accountId);
+
+console.log(accountCity);
+console.log(accountEmail);
+console.log(accountPassword);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
