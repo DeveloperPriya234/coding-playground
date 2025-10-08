10b32b66c9529721b4adf8cb2356c1d5e1f44f76
DeveloperPriya234 <developerpriya142@gmail.com>
2025-10-08 11:27:33 +0530

    datatypes

--- D.S.A/javascript/01_basics/04_comparision.js
+++ D.S.A/javascript/01_basics/04_comparision.js
@@ -10,8 +10,21 @@
 // console.log("02">1);
 
 
+
+
+
 console.log(null>0);
 console.log(null<0);
 console.log(null==0);
 console.log (null >= 0 );
-console.log(null<=0);
+console.log(null <=0 );
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+
+//=== 
+log.console("2"=== 2);
+
---
+++ D.S.A/javascript/01_basics/datatypes.summary.js
@@ -0,0 +1,17 @@
+// primitive
+// 7 types : string, number , boolean,null,undefined,symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+console.log(typeof  score )
+console.log(typeof  scoreValue )
+console.log(typeof  isLoggedIn )
+console.log(typeof  outsideTemp )
+console.log(typeof  userEmail )
+
+// Refrence (non primitive)
+
+// array,objects,functions
