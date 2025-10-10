761961fa70ae2355eaee7225fbd4631ac50d3e41
DeveloperPriya234 <developerpriya142@gmail.com>
2025-10-10 19:08:49 +0530

    js

--- D.S.A/javascript/01_basics/05_strings.js
+++ D.S.A/javascript/01_basics/05_strings.js
@@ -8,4 +8,17 @@ const gameName = new String(`hiteshhc`)
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice()
+
+
 
