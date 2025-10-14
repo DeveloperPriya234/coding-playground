086f54d930d2e299a4feed570bdace5b79e39c61
DeveloperPriya234 <developerpriya142@gmail.com>
2025-10-14 20:13:51 +0530

    js

--- D.S.A/javascript/01_basics/06_number_and_maths.js
+++ D.S.A/javascript/01_basics/06_number_and_maths.js
@@ -43,4 +43,12 @@ console.log(Math.floor(4.9));
 
 console.log(Math.random()); //always in range 0 to 1 all time number is change randomly;
 
+console.log(Math.floor(Math.random()*10)+1);
 
+
+const min = 10
+const max = 20
+
+
+
+Math.random()* (max-min + 1)+ min 
