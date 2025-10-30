2a17c5da2d58a671131563921225870ca1025c5b
DeveloperPriya234 <developerpriya142@gmail.com>
2025-10-30 14:46:51 +0530

    let var const

---
+++ D.S.A/New folder/02_var_let_const.js
@@ -0,0 +1,17 @@
+console.log("javascript tutorials 3: var, let, and const");
+// let a = 45; redeclared and update both allowed in var
+// let b = "p"
+let b = "harry"; // redeclaration not allowed in let 
+//let update but not redeclared
+const author = "harry";
+// let author = 5; const not allowed to change value
+//const  not update and not redeclared
+b =4
+let c = null;
+let d = undefined;
+{ 
+    let b = 'this'
+    console.log(b);
+}
+console.log(author);
+console.log(b)
---
+++ D.S.A/New folder/index.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+let a = 67
+a = "harry"
+
+console.log(a);
+// let 8harry = 7// not allowed this will throw an error
