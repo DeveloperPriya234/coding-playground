47fc5ebff17a71b56b2217d3a2b2d6b6758d907c
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-01 18:26:43 +0530

    operator and expresions

---
+++ D.S.A/New folder/chatpter1_practiceset/operator_expressions.js
@@ -0,0 +1,12 @@
+console.log("operators in js")
+let a = 98;
+let b = 56;
+console.log("a + b =",a+b);
+console.log("a - b =",a-b);
+console.log("a * b =",a*b);
+console.log("a / b =",a/b);
+console.log("a % b =",a%b);
+console.log("a ** b =",a**b);
+
+console.log("++a =",++a);
+
