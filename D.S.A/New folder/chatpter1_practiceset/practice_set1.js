a4b5acfda4087ea60fca0261da97f528bccc5841
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-01 18:21:08 +0530

    practice set 1

---
+++ D.S.A/New folder/chatpter1_practiceset/practice_set1.js
@@ -0,0 +1,42 @@
+// chapter 1 question 1
+let a = "harry";
+let b = 6;
+
+console.log(a+b);
+// chapter 1 question 2
+console.log(typeof (a+b));
+
+// chapter 1 question 3
+// const a1 = {
+//     name:"harry",
+//     section:"A",
+//     isPrincipal:false
+// }
+// a1 = 65
+
+// console.log(a1);
+
+
+// chapter 1 question 4
+
+const a2 = {
+    name:"harry",
+    section:1,
+    isPrincipal:false
+}
+a2["friend"]="rohan"
+console.log(a2);
+
+// chapter 1 question 5
+
+const dict = {
+    appreciate:"recognize the full worth of",
+    appropriate:"suitable or proper in the circumstances",
+    approval:"the action of approving something",
+    yakka: "work, especially hard work",
+    ataraxia:"a state of serene calmness"
+
+}
+
+console.log(dict.appreciate);
+console.log(dict["yakka"]);
