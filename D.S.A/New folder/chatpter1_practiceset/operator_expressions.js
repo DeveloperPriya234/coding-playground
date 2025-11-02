31d606220c2df2b9952a8ce9295d0b22f533cadb
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-02 12:32:58 +0530

    logical operators

--- D.S.A/New folder/chatpter1_practiceset/operator_expressions.js
+++ D.S.A/New folder/chatpter1_practiceset/operator_expressions.js
@@ -4,9 +4,63 @@ let b = 56;
 console.log("a + b =",a+b);
 console.log("a - b =",a-b);
 console.log("a * b =",a*b);
-console.log("a / b =",a/b);
+console.log("a/b =",a/b);
 console.log("a % b =",a%b);
 console.log("a ** b =",a**b);
-
 console.log("++a =",++a);
+console.log("--b =",--b);
+console.log("a++ =",a++);
+console.log("b-- =",b--);
+
+console.log("a =",a);
+console.log("b =",b);
+
+
+console.log("a==b :",a==b);
+console.log("a!=b :",a!=b);
+console.log("a>=b :",a>=b);
+console.log("a<=b :",a<=b);
+console.log("a>b :",a>b);
+console.log("a<b :",a<b);
+
+
+console.log( (a==b) && (a>b) );
+
+let comp1 = 43;
+let comp2 = 56;
+console.log("comp1 == comp2 ",comp1 == comp2 );
+console.log("comp1 == comp2 ",comp1 != comp2 );
+
+console.log("comp1 === comp2 ",comp1 === comp2 );//equal value and type
+console.log("comp1 ==! comp2 ",comp1 !== comp2 );
+
+
+//logical operator
+//true and true = true
+//true and false = false
+//false and true = false
+//false and false = false
+
+let x = 5;
+let y = 10;
+console.log( (x < 10) && (y > 5) ); // true && true = true
+console.log((x < 10) && (y < 5) ); // true && false = false
+console.log((x > 10) && (y > 5) ); // false && true = false
+console.log((x > 10) && (y < 5) ); // false && false = false
+
+//logical operator or ||
+
+//true or true = true
+//true or false = true
+//false or true = true
+//false or false = false
+
+
+console.log( (x < 10) || (y > 5) ); // true || true = true
+console.log((x < 10) || (y < 5) ); // true || false = true
+console.log((x > 10) || (y > 5) ); // false || true = true
+console.log((x > 10) || (y < 5) ); // false || false = false
+
+
+
 
