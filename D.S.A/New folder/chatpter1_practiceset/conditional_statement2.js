e226f3ead47b937cbbdcf86398ddaf4592581e8a
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-03 16:51:10 +0530

    consitional

---
+++ D.S.A/New folder/chatpter1_practiceset/conditional_statement2.js
@@ -0,0 +1,15 @@
+age = 56;
+if (age< 0){
+    console.log("this is invalid age");
+}
+else if (age<9){
+    console.log("you are a kid and you cannot even think of driving");
+}
+
+else if (age<18 && age>=9){
+    console.log("you are a teen and you can think of driving after 18");
+}
+
+else{
+    console.log("you are eligible for driving");
+}
