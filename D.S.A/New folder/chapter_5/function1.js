0c80208888788fdf4335285eb6572f36dd397200
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-12 12:43:17 +0530

    function

--- D.S.A/New folder/chapter_5/function1.js
+++ D.S.A/New folder/chapter_5/function1.js
@@ -14,10 +14,41 @@
 
 
 //parameters(a,b);function declaration
-//arguments(5,10);
+//arguments(5,10);function calling
 
 
-function addTwoNumbers(a, b) {
-console.log(a + b);
+// function addTwoNumbers(a, b) {
+// console.log(a + b);
+// }
+// addTwoNumbers(5, 10);
+
+
+/**
+ * The function named multiply takes two parameters, multiplies them together, and logs the result to
+ * the console.
+ * @param a - 5
+ * @param b - 10
+ */
+// function multiply(a,b){
+// console.log(a*b);
+// }
+// multiply(5,10);
+
+function add(num1,num2){
+    return num1+num2;
 }
-addTwoNumbers(5, 10);
+
+
+let result =add(5,10);
+console.log(result);
+
+
+//anonymous function// function name is not defined
+let  multiply = function(a,b){
+    return a*b;
+};
+let result2 = multiply(5,10);
+console.log(result2);
+
+
+
