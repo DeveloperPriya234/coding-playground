c9dbacdae39a17de66358cb483842a7caed9dbf3
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-16 12:17:57 +0530

    scope

---
+++ D.S.A/New folder/chapter6/scope_hashing1.js
@@ -0,0 +1,38 @@
+//scope 
+//local scope(function scope
+function test() {
+    var a = 30;
+    console.log(a); //30
+}
+test();
+//console.log(a); //ReferenceError: a is not defined
+//global scope
+var b = 40;
+console.log(b);
+
+
+
+
+
+
+//block scope
+
+if (true) {
+    let c = 50;
+    console.log(c); //50
+
+
+}//console.log(c); //ReferenceError: c is not defined
+for (let i = 0; i < 3; i++) {
+    console.log(i); //0 1 2
+}
+//console.log(i); //ReferenceError: i is not defined
+{
+    const d = 60;
+    console.log(d); //60
+}
+
+
+
+
+//console.log(d); //ReferenceError: d is not defined    
--- D.S.A/New folder/chapter_5/function_worksheet/question1.js
+++ D.S.A/New folder/chapter_5/function_worksheet/question1.js
@@ -1,10 +1,64 @@
-// function greet(){
-//     console.log("Hello, World!");
-// }
-
-function addtwonumber(a,b){
-    return a + b;
+// Write a function that returns the sum of two numbers.
+function add(a,b){
+    return a+b;
 }
+sum = add(3,5);
+console.log("Sum of two numbers is: " + sum);
 
-let sum = addtwonumber(20,87);;
-console.log(sum);
+
+// Write a function that checks if a number is even or odd.
+
+
+function isEvenOrOdd(num){
+    if(num %2==0)
+        return "Even";
+    else
+        return "Odd";
+}
+isEvenOrOddResult = isEvenOrOdd(7);
+console.log("The number is: " + isEvenOrOddResult);
+
+
+
+
+// Write a function that takes a name and returns "Hello, <name>!".
+function greet(name){
+    return "Hello, " + name + "!";
+}
+greeting = greet("Alice");
+console.log(greeting);
+// Write a function that returns the square of a number.
+function square_num(num){
+    return num ** 2;
+}
+squared = square_num(76);    
+console.log("Square of the number is: " + squared);
+
+// Write a function that converts Celsius to Fahrenheit.
+
+function celsiusToFahrenheit(celsius){
+    return (celsius * 9/5) + 32;
+
+}
+fahrenheit = celsiusToFahrenheit(25);
+console.log("Fahrenheit value is: " + fahrenheit);
+
+
+//Write a function that takes a user object and returns "First Last".
+
+function fullname(user){
+    return firstName + " " +lastName;
+
+}
+firstName = "John";
+lastName = "Doe";
+full_name = fullname();
+console.log("Full name is: " + full_name);
+
+
+function fullname(user){
+    return user.firstName + " " + user.lastName;
+}
+user = {firstName: "John", lastName: "Doe"};
+full_name = fullname(user);
+console.log("Full name is: " + full_name);
