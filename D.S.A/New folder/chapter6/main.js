462207c4a0c398a6b811deaca5a09af5213a0bb1
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-17 18:12:59 +0530

    global and local scope

--- D.S.A/New folder/chapter6/main.js
+++ D.S.A/New folder/chapter6/main.js
@@ -1,13 +1,40 @@
 //*outside of the function or block
+//! anything we add in var whose value add in window object
+// var a =10; // global scope(anywhere i can access it)
 
-var a =10;
+// function add(){
+//     let a = 10
+//     let b = 20
 
+// }
+
+// {
+//     var name = "priya"
+// }
+
+let a = 10;
+{
+    console.log(a);
+}
+
+if(10>9){
+    console.log(a);
+}
+
+
+//local scope
 function add(){
     let a = 10
     let b = 20
-
+    console.log(a+b);
 }
+add();
+// console.log(a+b); // error
 
 {
-    var name = "priya"
+    let name = "priya"
+    console.log(name);
 }
+// console.log(name); // error`
+
+
