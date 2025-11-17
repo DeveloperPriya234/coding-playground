e4664a5f134ac2f22c75da7fdae2f93a162ed1ec
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-17 19:40:19 +0530

    lexical scoping

---
+++ D.S.A/New folder/chapter6/lexical_scoping.js
@@ -0,0 +1,36 @@
+//lexical scoping;
+function outerFunction() {
+    let outerVariable = 'I am from outer function';
+
+
+    function innerFunction() {
+        let innerVariable = 'I am from inner function';
+        console.log(outerVariable); // Accessing outer function variable
+        console.log(innerVariable); // Accessing inner function variable
+    }
+    innerFunction();
+    // console.log(innerVariable); // Error: innerVariable is not defined
+
+
+}
+
+outerFunction();
+// console.log(outerVariable); // Error: outerVariable is not defined
+// console.log(innerVariable); // Error: innerVariable is not defined
+//lexical scoping with nested
+
+
+function parent(){
+    let ParentName = "I am from parent function";
+    function child(){
+        let childname = "I am from child function";
+        console.log(ParentName)
+        console.log(childname);
+    }
+
+    return child;
+}
+
+let childFunction = parent();
+childFunction(); // invoking child function
+
