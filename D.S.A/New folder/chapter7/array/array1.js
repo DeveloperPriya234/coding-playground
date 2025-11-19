88c34be43d5d4bdaf9768e445408fa91dcb297ee
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-19 20:32:15 +0530

    array

---
+++ D.S.A/New folder/chapter7/array/array1.js
@@ -0,0 +1,53 @@
+//array
+
+
+let arr = [10, 20, 30, 40, 50];
+console.log(arr);
+console.log(arr[2]); //accessing array element
+
+//modifying array element
+arr[2] = 35;
+console.log(arr);
+
+//array methods
+arr.push(60); //adds element at the end
+console.log(arr);
+arr.pop(); //removes element from the end
+console.log(arr);
+arr.unshift(5);
+
+
+//adds element at the beginning
+console.log(arr);
+arr.shift();
+//removes element from the beginning
+console.log(arr);
+//length of array
+console.log(arr.length);
+
+
+//looping through array
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+//array
+// let fruits =["apple","banana","orange"];
+let myarray =["priya",21,true,5.6];//* 1ways to create array
+let mynums = new Array(1,2,3,4,5);//* 2ways to create array
+let emptyarray = [];
+console.log(emptyarray);
+console.log(myarray);
+console.log(mynums);    
+
+let num = [10,20,30,40,50];
+console.log(num);
+console.log(num[2]); // accessing array element
+console.log(num.length); // length of array
+// modifying array element
+num[2] = 35;
+console.log(num);
+
+
+
+
+
