cfad18b7be7516715175c5ff86e068900766d310
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-23 11:35:38 +0530

    array

---
+++ D.S.A/New folder/hosting/hosting1.js
@@ -0,0 +1,26 @@
+function one (){
+    const username ="priyaa";
+    function two (){
+        const website ="youtube";
+        console.log(username);
+    
+    
+    }
+    
+two();
+}
+//one();
+
+if (true){
+    const username ="kumar";
+    if(username ==="kumar"){
+        console.log(`hello ${username}`);
+    }
+
+
+}
+
+
+
+
+
