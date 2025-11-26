ed836a6b0565c0971d18b11e26c32172973b6a6e
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-26 19:11:05 +0530

    array2 notes

--- D.S.A/New folder/chapter7/array2.js
+++ D.S.A/New folder/chapter7/array2.js
@@ -1,31 +1,33 @@
 
 // methods of array
 // push(),
-// .adds items(s) to the end of an array
-let num1 = [10,20,30];
-num1.push(67)
-console.log(num1);
+// !.adds items(s) to the end of an array
+// *let num1 = [10,20,30];
+// *num1.push(67)
+// *console.log(num1);
 
-//pop(),
-//.removes the last item from an array
-let result = num1.pop();
-console.log(result);
-console.log(num1);
+// //pop(),
+// !.removes the last item from an array
+//* let result = num1.pop();
+// *console.log(result);
+// *console.log(num1);
 
-//shift(),
-//.removes the first item from an array
-let shift = num1.shift();
-console.log(shift);
-console.log(num1);
+// //shift(),
+// !.removes the first item from an array
+// *let shift = num1.shift();
+// *console.log(shift);
+// *console.log(num1);
 
-//unshift(),
-//.adds items(s) to the beginning of an array
-let unshiftitem = num1.unshift(5);
-console.log(unshiftitem);
-console.log(num1);
+// unshift(),
+// !.adds items(s) to the beginning of an array
+//* let unshiftitem = num1.unshift(5);
+// *console.log(unshiftitem);
+// *console.log(num1);
 
-//length;
-console.log(num1.length);
+// length;
+//* console.log(num1.length);
 
 //splice(remove.replace.insert);
-//.remove items from an array
+//!.remove items from an array
+
+
