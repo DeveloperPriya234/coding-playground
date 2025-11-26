cef3a62b8e82b5f56c52816910b10009f74d7f4d
DeveloperPriya234 <developerpriya142@gmail.com>
2025-11-26 11:34:38 +0530

    array1

--- D.S.A/New folder/chapter7/array/array1.js
+++ D.S.A/New folder/chapter7/array/array1.js
@@ -1,55 +1,84 @@
-//array
+// //array
 
 
-let arr = [10, 20, 30, 40, 50];
-console.log(arr);
-console.log(arr[2]); //accessing array element
+// let arr = [10, 20, 30, 40, 50];
+// console.log(arr);
+// console.log(arr[2]); //accessing array element
 
-//modifying array element
-arr[2] = 35;
-console.log(arr);
+// //modifying array element
+// arr[2] = 35;
+// console.log(arr);
 
-//array methods
-arr.push(60); //adds element at the end
-console.log(arr);
-arr.pop(); //removes element from the end
-console.log(arr);
-arr.unshift(5);
-
-
-//adds element at the beginning
-console.log(arr);
-arr.shift();
-//removes element from the beginning
-console.log(arr);
-//length of array
-console.log(arr.length);
+// //array methods
+// arr.push(60); //adds element at the end
+// console.log(arr);
+// arr.pop(); //removes element from the end
+// console.log(arr);
+// arr.unshift(5);
 
 
-//looping through array
-for(let i=0;i<arr.length;i++){
-    console.log(arr[i]);
-}
-//array
-// let fruits =["apple","banana","orange"];
+// //adds element at the beginning
+// console.log(arr);
+// arr.shift();
+// //removes element from the beginning
+// console.log(arr);
+// //length of array
+// console.log(arr.length);
+
+
+// //looping through array
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+// //array
+// // let fruits =["apple","banana","orange"];
+// let myarray =["priya",21,true,5.6];//* 1ways to create array
+// let mynums = new Array(1,2,3,4,5);//* 2ways to create array
+// let emptyarray = [];
+// console.log(emptyarray);
+// console.log(myarray);
+// console.log(mynums);    
+
+// let num = [10,20,30,40,50];
+// console.log(num);
+// console.log(num[2]); // accessing array element
+// console.log(num.length); // length of array
+// console.log(num[2], num[4]); // accessing multiple elements
+// // modifying array element
+// num[2] = 35;
+// console.log(num);
+// console.log(num.length);
+
+
+
+
+
+
+
+let nums = [10,25,3948,3939,490];
+console.log(nums[4]);
+
+nums[3] = 4000;
+console.log(nums);
+console.log(nums.length);
+
+//first element = 0
+//last element = length-1
+
+
+
 let myarray =["priya",21,true,5.6];//* 1ways to create array
-let mynums = new Array(1,2,3,4,5);//* 2ways to create array
-let emptyarray = [];
-console.log(emptyarray);
 console.log(myarray);
-console.log(mynums);    
+console.log(myarray.length);
+console.log(myarray[myarray.length -1]);
 
-let num = [10,20,30,40,50];
-console.log(num);
-console.log(num[2]); // accessing array element
-console.log(num.length); // length of array
-console.log(num[2], num[4]); // accessing multiple elements
-// modifying array element
-num[2] = 35;
-console.log(num);
-console.log(num.length);
+let fruits =["apple", ,"orange"];
+fruits[1] = 20;
+console.log(fruits);
 
+console.log(fruits[100]);
+fruits[50] ="banana";
+console.log(fruits[50]);
 
-
-
-
+let arr = [1,2,3,4,[0,1],["hello priiyaa"   ,"world"]];
+console.log(arr);
