c51053a6bedc607ce221d12420928157137f8d76
DeveloperPriya234 <developerpriya142@gmail.com>
2025-12-04 13:06:52 +0530

    object

---
+++ D.S.A/New folder/chapter10/object1.js
@@ -0,0 +1,29 @@
+// object 
+// !in javascript, **objects** are collections of key- value pairs.
+//!keys are always strings (or symbol),and values can be any type...
+
+//* let myPerson = {
+//  *   name:"priyaa",
+//    * age:21,
+//   *  isInCollege:true,
+//    * address:{
+//       *  city:"noida",
+//      *   nearBy:"metro station",
+//   *  },
+//   *  hobbies:["gym","walking","coffee"],
+//   *  greet:function(){
+//   *  console.log("hello world")
+// *}
+// *}
+
+// *myPerson.diet = ["maggie","fast food", "mommos","noodles","pizza"]
+ //1st way
+// ?console.log(myPerson) 
+// ?console.log(myPerson.greet())
+// ?console.log(myPerson.hobbies) 
+// ?console.log(myPerson.address.city) 
+
+
+//2nd way
+//? console.log(myPerson["hobbies"])
+
