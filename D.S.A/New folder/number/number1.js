81afc29e6dc47d719d31d9da49dd5a636def0851
DeveloperPriya234 <developerpriya142@gmail.com>
2025-12-09 21:07:14 +0530

    methods(round,floor,ceil abs.squareroot,power,rnadom.max.min)

--- D.S.A/New folder/number/number1.js
+++ D.S.A/New folder/number/number1.js
@@ -68,9 +68,9 @@
 //* console.log(Number.isNaN("abc"))
 //!finite
 
-console.log(isFinite(100))
-console.log(Number.isFinite("100"))
-console.log(Number.isFinite(12.333))
+// *console.log(isFinite(100))
+// *console.log(Number.isFinite("100"))
+// *console.log(Number.isFinite(12.333))
 
 
 //!integer
@@ -84,5 +84,68 @@ console.log(Number.isFinite(12.333))
 
 
 //?methods
-//* let n = 3.1415914159;
+//!to fixed
+// *let n = 3.1415914159;
 // *console.log(n.toFixed(9))
+// *let m = 1234456789;
+// *console.log(m.toFixed(8))
+
+//* let p = 3.124;
+// *console.log(p.toFixed(2))
+
+//!to precision
+//?return a string with a total of n significant digits
+//* let num = 1233456.79
+// *console.log(num.toPrecision(2))
+
+//!to exponential
+//* let n = 12345;
+// *console.log(n.toExponential(1))
+
+//! to string
+// *let n = 23;
+// *let str = n.toString();
+// *console.log(str);
+// *console.log(typeof str)
+
+// *let completed = 5>2;
+// *console.log(completed.toString())
+// *console.log(typeof(completed))
+
+//* let complete = 6>2;
+// *let newString = (complete.toString())
+// *console.log(typeof(newString))
+
+
+//valueOf()
+//* let number = 50;
+// *let newNumber = (number.valueOf())
+// *console.log(newNumber)
+
+
+//!round
+
+// *console.log(Math.round(4.9))
+// *console.log(Math.round(65.87))
+
+//!floor
+//* console.log(Math.floor(3.65));
+//!SquareRoot
+//* console.log(Math.sqrt(169))
+
+//!power(pow)
+
+// *console.log(Math.pow(2, 3))
+
+// *console.log(Math.pow(3,  7))
+//!random
+
+// *console.log(Math.random())
+
+//!max
+// *console.log(Math.max(2,3,4,56,))
+
+
+//!min
+// *console.log(Math.min(1,2,3,4,5))
+
