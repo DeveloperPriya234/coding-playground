85663fdfec992bddf5189236a27ffdb6e8f290ce
DeveloperPriya234 <developerpriya142@gmail.com>
2025-12-09 13:08:59 +0530

    number,max,in

---
+++ D.S.A/New folder/number/number1.js
@@ -0,0 +1,35 @@
+//number
+//!javascript has only one number type: number (IEEE 754 double-precision 64-bit binary format).
+//?number can be:
+//?integers 42
+//?floats(decimals):3.14
+//?scientific notation:1.5e3(i.e.,1500)
+
+//number
+//* let num1 = 10
+// *let num2 = (2.33)
+// *let num3 = 2.5e6
+// *console.log(typeof(num1))
+// *console.log(typeof(num2))
+// *console.log(typeof(num3))
+
+//?maths
+// *let inif = Infinity;
+// *console.log(inif)
+
+
+//* let negInig = -Infinity;
+//* console.log(negInig)
+
+// *let nan = NaN;
+// *console.log(nan)
+
+
+//max value
+//* const max = Number.MAX_VALUE;
+// *console.log(max)
+
+
+// *const min = Number.MIN_VALUE;
+// *console.log(min)
+
