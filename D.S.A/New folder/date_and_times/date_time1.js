c4a672949e5d06b125bd05e46e42a332b51b0d4a
DeveloperPriya234 <developerpriya142@gmail.com>
2025-12-10 13:21:21 +0530

    date and time

--- D.S.A/New folder/date_and_times/date_time1.js
+++ D.S.A/New folder/date_and_times/date_time1.js
@@ -1,5 +1,71 @@
-let now = new Date();
-console.log(now)
+// let now = new Date();
+// console.log(now)
 
-let date2 = new Date(2025, 11,  11)
-console.log(date2)
+// let date2 = new Date(2025, 11,  11)
+// console.log(date2)
+
+
+
+
+
+// let date4 = new Date(0);
+// console.log(date4)
+
+
+// // 1sec = 1000 milli second
+
+// let date5 = new Date(1000*60*60)
+
+// console.log(date5)
+
+
+//!2. date internals
+
+// let timestamp = Date.now()
+// let d = new Date(timestamp)
+
+// console.log(d)
+// //*get method
+// ?console.log(d.getMonth())
+
+// ?console.log(d.getFullYear())
+// ?console.log(d.getDate())
+
+// ?console.log(d.getHours())
+//?? console.log(d.getMinutes())
+
+// ?console.log(d.getTime())
+// ?console.log(d.getTimezoneOffset())
+// ?console.log(d.getUTCDate)
+// ?console.log(d.getDay())
+// ?console.log(d.toDateString)
+
+
+// let date = new Date()
+// console.log(date.setMonth(3))
+
+// console.log(date)
+
+
+
+// let d1 = new Date("2025-05-15");
+// let d2 = new Date("2025-05-20")
+
+// console.log(d1>d2)
+// console.log(d1<d2)
+
+
+// let d = new Date();
+// let tomorrow = new Date(d.getTime()+ 24 *
+// 60*60*1000);//add 1 day
+// let lastWeek = new Date(d.getTime()-7*24*60*60*1000);
+
+// console.log(tomorrow.toLocaleDateString)
+// console.log(lastWeek.toLocaleString)
+
+
+// let d = new Date()
+// console.log(d.getUTCFullYear())
+
+
+
