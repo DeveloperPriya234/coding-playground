ee0d6909115ac14d9b12bc5a434321a7cb32e88b
DeveloperPriya234 <developerpriya142@gmail.com>
2025-12-12 12:27:44 +0530

    JSON

---
+++ D.S.A/intermediate js/jsadvance/chapter1.js
@@ -0,0 +1,23 @@
+//json
+//!json javascript object notation is a interchange format that is:
+//?human-readable
+//?easy for machines to parse and generate
+//? language independent (although it is based on an subset of javascript syntax)
+
+//*example
+
+
+// {
+//     "name":"priya",
+//     "age":21,
+//     "isStudent":true,
+//     "skills":["javascript","python"],
+//     "address":{
+//         "city":"delhi",
+//         "pincode":110001
+
+    
+//     }
+
+
+// }
